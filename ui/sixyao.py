bf821db8da1e56a2bb938977bf2096bfb1d64ee2
jerry-harm <jerry_harm@163.com>
2026-02-24 10:25:36 +0800

    chore: :page_facing_up: 协议添加

--- ui/sixyao.py
+++ ui/sixyao.py
@@ -1,3 +1,20 @@
+'''
+    六爻组件
+    Copyright (C) 2026  jerry <jerry_harm@163.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
